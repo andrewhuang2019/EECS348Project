114b7567f57b919a488b65db48740458661b9f93
katharineswann <157732341+katharineswann@users.noreply.github.com>
2024-04-20 18:57:56 -0500

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {}
+
+string orFunction(string expression){
+    if somepart.expression == "T";
+        then front = true;
+    if somepart.expression == "T";
+        then back = true;
+
+    if front OR back;
+        string ans = ""
+    return ans;
+}
+
+string andFunction(string expression){
+    return "S";
+}
+
+string notFunction(string expression){
+    return "S";
+}
+
+string nandFunction(string expression){
+    return "S";
+}
+
+string xorFunction(string expression){
+    return "S";
+}
