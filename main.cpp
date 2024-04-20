ac16a12841b20be230565d6240da85dbd50ad8b8
Andysword <andrewhuang2019@gmail.com>
2024-04-20 18:54:11 -0500

    Created readInput
    Created function that reads the user input from the terminal.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+void readInput();
+
 int main(){
+    readInput();
+    return 0;
+}
+
+void readInput(){
+    string userInput;
+    cin >> userInput;
+    cout << userInput << endl;
+
     
+
 }
