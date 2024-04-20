6bee334b2b342eb52c28a1a9d601ffc3c44520ae
Hunter Long <hwolong@gmail.com>
2024-04-20 18:55:58 -0500

    added isValid function

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <stdbool.h>
 using namespace std;
 
+char operators[] = {'&','|','!','@','$'}
+
+bool isValid(string input) {
+    int openParen = 0;
+    int closeParen = 0;
+    for (int i = 0; i < sizeof(input); i++) {
+        if (input[i] == '(') {
+            openParen++;
+        }
+        if (input[i] == ')') {
+            closeParen++;
+        }
+    }
+    if (openParen != closeParen) {
+        return false;
+    }
+    return true;
+}
+
 int main() {
-    
+
 }
