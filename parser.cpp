48dba87541c340aeb54edee7f901b4f07f52ef1d
Hunter Long <hwolong@gmail.com>
2024-04-20 20:43:24 -0500

    lots of work on parenHandler

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <stdbool.h>
+#include <map>
 #include <algorithm>
 using namespace std;
 
@@ -17,6 +18,69 @@ string Parser::removeWhitespace(string expr){
     string::iterator end_pos = remove(expr.begin(), expr.end(), ' ');
     expr.erase(end_pos, expr.end());
     return expr;
+}
+/*
+string parenthesesHandler(string expr) {
+    Parser parse = Parser();
+    int openParen = 0;
+    int closeParen = 0;
+    int index = 0;
+    int openIndex;
+    for (int i = 0; i < sizeof(expr); i++) {
+        if (expr[i] == '(') {
+            openParen++;
+            if (openParen == 1) {
+                openIndex = index;
+            }
+        }
+        if (expr[i] == ')') {
+            closeParen++;
+        }
+        index++;
+        if (openParen == closeParen && openParen != 0) {
+            break;
+        }
+    }
+    if (openParen == 0) {
+        if (parse.isValid(expr)) {
+            
+        };
+    } else {
+        string subParen = expr.substr(openIndex,index);
+        return parenthesesHandler(subParen);
+    }
+    
+}
+*/
+
+string parenthesesHandler(string expr) {
+    map<int, int> parenCount;
+    int openParen = 0;
+    int closeParen = 0;
+    for (int i = 0; i < sizeof(expr); i++) {
+        if (expr[i] == '(') {
+            openParen++;
+        }
+        if (expr[i] == ')') {
+            closeParen++;
+        }
+        parenCount[i] = openParen - closeParen;
+    }
+    int maxDepth = 0;
+    for (int i = 0; i < sizeof(expr); i++) {
+        if (parenCount[i] > maxDepth) {
+            maxDepth = parenCount[i];
+        }
+    }
+    int startIndex = 0;
+    for (int i = 0; i < sizeof(expr); i++) {
+        if (parenCount[i] == maxDepth) {
+            break;
+        }
+        startIndex++;
+    }
+    int endIndex = startIndex;
+    while 
 }
 
 bool Parser::isValid(string expr) {
@@ -36,7 +100,7 @@ bool Parser::isValid(string expr) {
                         return false;
                     }
                 }
-                if (expr[i+1] == '(' || expr[i+1] == ')') {
+                if (expr[i+1] == '(' || expr[i+1] == ')' || expr[i+1] == ' ') {
                     return false;
                 }
             }
