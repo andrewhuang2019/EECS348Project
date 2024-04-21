b06f417561e76e1e50ac091692511f7e2cb95f13
Katharine Swann <katharinesswann@gmail.com>
2024-04-20 19:36:41 -0500

    completed operator.cpp
    main, or, and, and not function

--- operators.cpp
+++ operators.cpp
@@ -2,26 +2,84 @@
 #include <string>
 using namespace std;
 
-int main() {}
+int main() {
+    string test1 = "T|F";
+    orFunction(test1);
+
+    string test2 = "T&F";
+    andFunction(test2);
+
+    string test3 = "T!F";
+    notFunction(test3);
+
+    string test4 = "T@F";
+    nandFunction(test4);
+
+    string test5 = "T$F";
+    xorFunction(test5);
+}
 
 string orFunction(string expression){
-    if somepart.expression == "T";
-        then front = true;
-    if somepart.expression == "T";
-        then back = true;
+    string ans = "";
+    if(expression[0] == 'T' || expression[2] == 'T'){
+        ans = "T";
+    }
 
-    if front OR back;
-        string ans = ""
+    if(expression[0] == 'F' || expression[2] == 'T'){
+        ans = "T";
+    }
+
+    if(expression[0] == 'T' || expression[2] == 'F'){
+        ans = "T";
+    }
+
+    if(expression[0] == 'F' || expression[2] == 'F'){
+        ans = "F";
+    }
+
     return ans;
 }
 
 string andFunction(string expression){
+    string ans = "";
+    if(expression[0] == 'T' && expression[2] == 'T'){
+        ans = "T";
+    }
 
-    return "S";
+    if(expression[0] == 'F' && expression[2] == 'T'){
+        ans = "F";
+    }
+
+    if(expression[0] == 'T' && expression[2] == 'F'){
+        ans = "F";
+    }
+
+    if(expression[0] == 'F' && expression[2] == 'F'){
+        ans = "T";
+    }
+
+    return ans;
 }
 
 string notFunction(string expression){
-    return "S";
+    string ans = "";
+    if(expression[0] == 'T' && !(expression[2] == 'T')){
+        ans = "F";
+    }
+
+    if(expression[0] == 'F' && !(expression[2] == 'T')){
+        ans = "T";
+    }
+
+    if(expression[0] == 'T' && !(expression[2] == 'F')){
+        ans = "T";
+    }
+
+    if(expression[0] == 'F' && !(expression[2] == 'F')){
+        ans = "F";
+    }
+
+    return ans;
 }
 
 string nandFunction(string expression){
