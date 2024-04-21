e8a1374d382a31e5a12b6f394dcb2655a072cd3e
Keaton Xu <keatx@ku.edu>
2024-04-20 19:20:42 -0500

    Update parser.cpp
    Moved function definitions outside of class

--- parser.cpp
+++ parser.cpp
@@ -3,56 +3,42 @@
 #include <stdbool.h>
 using namespace std;
 
-
-
 class Parser {
-
     private:
-    char operators[5] = {'&','|','!','@','$'};
+        char operators[5] = {'&','|','!','@','$'};
 
     public:
+        string removeWhitespace(string expr);
+        bool isValid(string input);
+};
 
-    int main() {
-        //example of getting user input and removing whitespace
-        string expr;
-        getline(cin, expr);
-        expr = removeWhitespace(expr);
-        cout << expr;
-        return true;
-    }
+string Parser::removeWhitespace(string expr){
+    string::iterator end_pos = remove(expr.begin(), expr.end(), ' ');
+    expr.erase(end_pos, expr.end());
+    return expr;
+}
 
-    string removeWhitespace(string expr);
-    bool isValid(string input);
-
-    bool isValid(string expr) {
-        int openParen = 0;
-        int closeParen = 0;
-        for (int i = 0; i < sizeof(expr); i++) {
-            if (expr[i] == '(') {
-                openParen++;
-            }
-            if (expr[i] == ')') {
-                closeParen++;
-            }
-            for (int j = 0; j < sizeof(operators); j++) {
-                if (expr[i] == operators[j]) {
-                    for (int k = 0; k < sizeof(operators); k++) {
-                        if (expr[i+1] == operators[k]) {
-                            return false;
-                        }
-                    }
-                    if (expr[i+1] == '(' || expr[i+1] == ')') {
+bool Parser::isValid(string expr) {
+    int openParen = 0;
+    int closeParen = 0;
+    for (int i = 0; i < sizeof(expr); i++) {
+        if (expr[i] == '(') {
+            openParen++;
+        }
+        if (expr[i] == ')') {
+            closeParen++;
+        }
+        for (int j = 0; j < sizeof(operators); j++) {
+            if (expr[i] == operators[j]) {
+                for (int k = 0; k < sizeof(operators); k++) {
+                    if (expr[i+1] == operators[k]) {
                         return false;
                     }
+                }
+                if (expr[i+1] == '(' || expr[i+1] == ')') {
+                    return false;
                 }
             }
         }
     }
-
-    string removeWhitespace(string expr){
-        string::iterator end_pos = remove(expr.begin(), expr.end(), ' ');
-        expr.erase(end_pos, expr.end());
-        return expr;
-    }
-
-};
+}
